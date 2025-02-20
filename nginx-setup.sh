7db0546838e44ba14857f26e2cf4a5587fdb2ebe
popper <sir.popper@gmail.com>
2025-02-20 17:48:22 -0600

    added cronjob

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -16,3 +16,4 @@ sudo echo "server {
 }" > /etc/nginx/sites-available/$1
 sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
 sudo certbot --nginx -d $1.netalert.ninja
+(sudo crontab -l 2>/dev/null; echo "0 0 * * * /usr/bin/tar -cvf "/root/backups/html-$(date +"\%m-\%d-\%Y").tar" -C /var/www/ html > /dev/null 2>&1; /usr/bin/find /root/backups/ -mtime +8 -name '*.tar' -exec /usr/bin/rm {} \;; /usr/bin/rm -rf /var/www/html/*; /usr/bin/cp -r /home/ubuntu/op/public/* /var/www/html/; /usr/bin/chown -R www-data:www-data /var/www/html/; /usr/bin/systemctl restart nginx;") | sudo crontab -
