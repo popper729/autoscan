89d25891e02b6576227f286ded7c5300bf3cbd94
popper <sir.popper@gmail.com>
2025-02-20 18:06:07 -0600

    bugfixing

--- nginx-setup.sh
+++ nginx-setup.sh
@@ -3,7 +3,6 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt install -y nginx certbot python3-certbot-nginx
 sudo systemctl enable nginx
-#sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/$1
 sudo echo "server {
 
 	root /var/www/html;
@@ -15,5 +14,6 @@ sudo echo "server {
 	}
 }" > /etc/nginx/sites-available/$1
 sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/$1
+sudo rm /etc/nginx/sites-enabled/default
 sudo certbot --nginx -d $1.netalert.ninja
 (sudo crontab -l 2>/dev/null; echo "0 0 * * * /usr/bin/tar -cvf "/root/backups/html-$(date +"\%m-\%d-\%Y").tar" -C /var/www/ html > /dev/null 2>&1; /usr/bin/find /root/backups/ -mtime +8 -name '*.tar' -exec /usr/bin/rm {} \;; /usr/bin/rm -rf /var/www/html/*; /usr/bin/cp -r /home/ubuntu/op/public/* /var/www/html/; /usr/bin/chown -R www-data:www-data /var/www/html/; /usr/bin/systemctl restart nginx;") | sudo crontab -
